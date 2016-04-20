e91d6ae2a229057d816359931edbb846df05fb3c
joeysab <josephsabbagh@live.com>
2016-04-20 18:49:03 +0000

    init commit

--- reddit.js
+++ reddit.js
@@ -1,11 +1,27 @@
 var request = require('request');
+var util = require("util")
 
-/*
-This function should "return" the default homepage posts as an array of objects
-*/
-function getHomepage(callback) {
-  // Load reddit.com/.json and call back with the array of posts
+
+//Function to expand on Objects within the console.log
+function betterLog(value) {
+  console.log(require('util').inspect(value, {
+    depth: 20,
+    colors: true
+  }));
 }
+
+
+//This function should "return" the default homepage posts as an array of objects
+function getHomepage(callback) {
+  var homepageAddress = "https://www.reddit.com/.json"; // Load reddit.com/.json and call back with the array of posts
+  request(homepageAddress, function(err, result) {
+    var homepageObj = JSON.parse(result.body)
+    callback(homepageObj.data.children);
+  });
+}
+getHomepage(function(obj) {
+  betterLog(obj)
+});
 
 /*
 This function should "return" the default homepage posts as an array of objects.
@@ -14,6 +30,11 @@ In contrast to the `getHomepage` function, this one accepts a `sortingMethod` pa
 function getSortedHomepage(sortingMethod, callback) {
   // Load reddit.com/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
+  var homepageAddress = "https://www.reddit.com/.json"; // Load reddit.com/.json and call back with the array of posts
+  request(homepageAddress, function(err, result) {
+    var homepageObj = JSON.parse(result.body)
+    callback(homepageObj.data.children);
+  });
 }
 
 /*
